c28b232b5211ae84ec30fa35cdd5f16526519819
Dmitry V. Luciv <dluciv@dluciv.name>
2017-02-27 08:12:38 +0300

    spaces/tabs arrrrr

--- bookmarks.js
+++ bookmarks.js
@@ -452,8 +452,8 @@ function handleParams () {
     }
     switch(parm("op")) {
     case "bookmark":
-	$('#newName').prop('value', parm("title"));
-	$('#newLink').prop('value', parm("url"));
+        $('#newName').prop('value', parm("title"));
+        $('#newLink').prop('value', parm("url"));
         $('#add_bookmark').get(0).scrollIntoView();
 	break;
     default:
